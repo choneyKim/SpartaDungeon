b823ebe40b57fd85f84baf0aa5b4ec0a74435ef9
yongwookhong <sinabro.yong@gmail.com>
2024-01-09 19:29:30 +0900

    item fix
    스펠 오류
    스페이스 지정

---
+++ SpartaDungeon/InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.collections.Genric;
+
+namespace Txt_Game
+
+    public class InventoryManager
+{
+    private List<Item> Items { get; private set; }
+
+    public InventoryManager() 
+    
+    {
+        Items = new List<Item>();
+    }
+    public void AddItem(Item item)
+    {
+        Items.Add(item);
+    }
+    public void RemoveItem(Item item)
+    { 
+        Items.Remove(item); 
+    }
+
+    public void DisplayInventory()
+    {
+        Console.WriteLine("\n");
+
+    }
+}

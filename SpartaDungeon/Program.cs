a86c7ccb5e9e19e188a8dd646c1526e7c5ed6e1f
yongwookhong <sinabro.yong@gmail.com>
2024-01-09 16:27:44 +0900

    Update class Item
    아이템 클래스를 추가했습니다

--- SpartaDungeon/Program.cs
+++ SpartaDungeon/Program.cs
@@ -8,7 +8,16 @@
 
 class Item
 {
+    public string Name { get; set; } 
+    public int Price { get; set; }
+    public string Description { get; set; }
 
+    public Item(string name, int price, string description)
+    {
+        Name = name; 
+        Price = price; 
+        Description = description;
+    }
 }
 
 class Player

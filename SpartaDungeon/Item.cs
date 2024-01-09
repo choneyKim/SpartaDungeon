045f2a3ac20833fd0239775c6bd54da77e8741d9
yongwookhong <sinabro.yong@gmail.com>
2024-01-09 17:49:17 +0900

    Create Item.cs
    item cs 생성

---
+++ SpartaDungeon/Item.cs
@@ -0,0 +1,16 @@
+﻿namesapce Txt_Game
+{
+    public class Item
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public string Description { get; set; }
+
+    public Item(string name, int price, string description)
+    {
+        Name = name;
+        Price = price;
+        Description = description;
+    }
+}
+}

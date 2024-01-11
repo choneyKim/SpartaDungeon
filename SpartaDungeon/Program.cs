cae1c4265da501c16cf45fe04396cc89c5ae6e91
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-01-11 12:39:54 +0900

    ㅈ

--- SpartaDungeon/Program.cs
+++ SpartaDungeon/Program.cs
@@ -202,22 +202,9 @@ class Shop
             Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
             Program.ShowHighlightedText_D("+++++++++++++++++++++++++++++++");
             Console.WriteLine("");
-
             Program.PrintTextWithHighlights("[ ","보유 골드","]");
             Console.WriteLine( p.Gold + "G" );
-            shopInven.DisplayInventory();
-
-            //Console.WriteLine("[보유 골드]");
-            //Console.WriteLine(p.Gold + "G");
-
-            //for (int i = 0; i < shopInven.Count(); i++)
-            //{
-            //    Item item = shopInven.ItemAccess(i);
-            //    Console.WriteLine($" {item.Name} | 가격: {item.Price} G | {item.Description}");
-            //}
-
-
-            //shopInven.DisplayShopInventory();
+            shopInven.DisplayShopInventory();
 
             Console.WriteLine("");
             Program.Firstlettercolor("1."," 아이템 구매");
